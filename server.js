7a2b62bd97f77c15b4f3da7c9ba4b6cd166a60c9
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-02-27 13:17:01 -0700

    server and router created

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+// const PeopleRouter = require('./people/router.js')
+
+const server = express();
+
+server.use(express.json());
+// server.use("/api/people", PeopleRouter);
+
+server.get("/", (req, res) => {
+    res.json({server: "up and running"})
+})
+
+module.exports = server
